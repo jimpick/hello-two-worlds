6debbcd51583b8908bdccb8162fed941ec69fbc3
Jim Pick <jim@jimpick.com>
2010-08-18 12:58:11 -0700

    update

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World from Joyent NodeKO!\n');
-}).listen(80, "127.0.0.1");
+}).listen(80);
 
