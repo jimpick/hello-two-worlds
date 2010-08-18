52034b7ab5fb1d89ef78e2ed7f84a30795c3ebae
Jim Pick <jim@jimpick.com>
2010-08-18 12:55:30 -0700

    Initial

---
+++ server.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World from Joyent NodeKO!\n');
+}).listen(80, "127.0.0.1");
+
