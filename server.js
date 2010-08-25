f4f22d6e938ae384b4e8c8603bdb8bb684133f3d
Jim Pick <jim@joyent.com>
2010-08-24 22:18:17 -0700

    Update.
    Just to see timestamp.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+// This needs a comment.
+
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World from Joyent NodeKO!\n');
+  res.end('Hello World from Joyent NodeKO! - new date\n');
 }).listen(80);
 
