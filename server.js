0379898b81f042bf3a5b6e707fe2664815a1d11d
Jim Pick <jim@joyent.com>
2010-08-28 11:37:27 -0700

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http'),
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'})
-  res.write("<h1>Jim Pick's Super Duper Node.js App</h1>")
+  res.write("<h1>Jim Pick's Super Duper Node.js App #2</h1>")
 });
 
 server.listen(parseInt(port))
