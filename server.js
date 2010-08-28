beb2c9d02d921c42957f8a66740e263e34d2e36f
Jim Pick <jim@joyent.com>
2010-08-28 11:40:12 -0700

    Change server.js to one from tutorial

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var http = require('http'),
-    port = process.env.PORT || 8001
+    port = parseInt(process.env.PORT) || 8001 
 
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  res.write("<h1>Jim Pick's Super Duper Node.js App #2</h1>")
-});
-
-server.listen(parseInt(port))
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Hello World\n')
+}).listen(port)
